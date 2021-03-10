b37b154bbab8fb23abf721ce1664d1f2d77365ed
udden89 <udd.marcus@gmail.com>
2021-03-10 19:58:29 +0100

    Start of the footer
    The start of building the footer

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+
+$('body').append(`
+
+<footer>
+  <div class="footer_main">
+    <div class="footer_right_section">
+      <div class="logo_container">
+        <img src="/img/logo/Filmvisarna logo 2.png" alt="Logotype">
+      </div>   
+    </div>
+  </div>
+</footer>
+
+`);
+
+
