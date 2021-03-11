96af3a7d4629437cc3ddc1f498f72414032d4b9c
udden89 <udd.marcus@gmail.com>
2021-03-11 19:08:34 +0100

    updated the footer with a menu

--- js/footer.js
+++ js/footer.js
@@ -4,14 +4,25 @@ $('body').append(`
 <footer>
   <div class="footer_container">
     <div class="footer_logo_container">      
-        <img src="/img/logo/Filmvisarna logo 2.png" alt="Logotype">     
-    </div>
-    <div class="copyRight_container">
+        <img src="/img/logo/Filmvisarna logo 2.png" alt="Logotype">
+        <div class="copyRight_container">
         © 2020 Filmvisarna AB. All rights reserved.
+        </div>
+    </div>    
+    <div class="footer_menu_container">
+      <ul>
+        <li><a href="#">Om oss</a></li>
+        <li><a href="#">Q&A</a></li>
+        <li><a href="#">Hitta hit</a></li>
+        <li><a href="#">Kontakt</a></li>
+      </ul>
+    </div>
+    <div class="footer_socialmedia_container">
+      <a href="#"><img src="/img/logo/facebook.png"></a>
+      <a href="#"><img src="/img/logo/instagram.png"></a>
+      <a href="#"><img src="/img/logo/twitter.png"></a>
     </div>
   </div>
 </footer>
 
 `);
-
-
