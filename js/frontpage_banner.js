df9bee3b5bcc84dc81613c0bea9fed8abd49b6c3
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-11 20:30:07 +0100

    Managed to get access to movies.json

--- js/frontpage_banner.js
+++ js/frontpage_banner.js
@@ -1,10 +1,25 @@
 
-$('body').append(`
+let bannerImages;
+
+getImgFromJson()
+
+async function getImgFromJson() {
+  bannerImages = await $.getJSON('/json/movies.json');
+  let test = bannerImages[3].title
+  console.log('111 '+ test)
+}
+
+
+
+/*$('body').append(`
+
 <div class="banner-Container">
+
 <div class="banner-img"></div>
-<div class="banner-text"><h1>Här är vår front banner</h1>
+
+<div class="banner-text"><h1>Hello</h1>
 <p>Innuti denna ska vi lägga en bild som en nice titel och brödtext med en beskrivning om filmen</p></div>
 
 
 </div>
-`)
+`)*/
