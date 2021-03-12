5c7505122b12dc7413d1abb809f896379f3f5cd5
udden89 <udd.marcus@gmail.com>
2021-03-12 12:29:05 +0100

    Added arrow up "button"
    But isnt 100% showing as intendent

--- js/footer.js
+++ js/footer.js
@@ -3,13 +3,13 @@ $('body').append(`
 
 <footer>
   <div class="footer_container">
-    <div class="footer_logo_container">      
+    <div class="footer_logo">      
         <img src="/img/logo/Filmvisarna logo 2.png" alt="Logotype">
-        <div class="copyRight_container">
+        <div class="copyRight">
         © 2020 Filmvisarna AB. All rights reserved.
         </div>
     </div>    
-    <div class="footer_menu_container">
+    <div class="footer_menu">
       <ul>
         <li><a href="#">Om oss</a></li>
         <li><a href="#">Q&A</a></li>
@@ -17,11 +17,15 @@ $('body').append(`
         <li><a href="#">Kontakt</a></li>
       </ul>
     </div>
-    <div class="footer_socialmedia_container">
+    <div class="footer_socialmedia">
       <a href="#"><img onmouseover="this.src='/img/logo/facebook30x30-red.png'" onmouseout="this.src='/img/logo/facebook30x30.png'" src="/img/logo/facebook30x30.png"></a>
       <a href="#"><img onmouseover="this.src='/img/logo/Instagram30x30-red.png'" onmouseout="this.src='/img/logo/Instagram30x30.png'" src="/img/logo/Instagram30x30.png"></a>
       <a href="#"><img onmouseover="this.src='/img/logo/Twitter30x30-red.png'" onmouseout="this.src='/img/logo/Twitter30x30.png'" src="/img/logo/Twitter30x30.png"></a>
     </div>
+    <div class="arrowUp">
+      <a href="#"><img onmouseover="this.src='/img/logo/arrowUp40x40-red.png'" onmouseout="this.src='/img/logo/arrowUp40x40.png'" src="/img/logo/arrowUp40x40.png"></a>
+    </div>
+    
   </div>
 </footer>
 
