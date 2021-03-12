d04388ef25b16db4a00b405743ed4a59d7622c71
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-12 13:18:35 +0100

    Managed to get json info out on the page

--- js/frontpage_banner.js
+++ js/frontpage_banner.js
@@ -1,25 +1,50 @@
-
 let bannerImages;
+let bannerTitle;
+let bannerText;
 
 getImgFromJson()
 
 async function getImgFromJson() {
-  bannerImages = await $.getJSON('/json/movies.json');
-  let test = bannerImages[3].title
-  console.log('111 '+ test)
+  movieInfo = await $.getJSON('/json/movies.json');
+  
+    renderInfo(movieInfo);
+  };
+ 
+
+
+function renderInfo() {
+ 
+  $('body').append(`
+
+  <div class="banner-Container">
+  <div class="banner-img"><img src="${movieInfo[0].images}"></div>
+  <div class="banner-text">
+  <div class="banner-title">${movieInfo[0].title}</div>
+  <div class="banner-desc">${movieInfo[0].description}</div>
+</div>
+
+</div>
+
+`)
+
 }
 
 
 
-/*$('body').append(`
+// $('body').append(`
 
-<div class="banner-Container">
+// <div class="banner-Container">
+// <div class="banner-img"></div>
 
-<div class="banner-img"></div>
+// <div class="banner-text">
+// <div class="banner-title"></div>
+// <div class="banner-desc"></div>
+// </div>
 
-<div class="banner-text"><h1>Hello</h1>
-<p>Innuti denna ska vi lägga en bild som en nice titel och brödtext med en beskrivning om filmen</p></div>
+// </div>
 
+// `)
 
-</div>
-`)*/
+  //  bannerImages = movies[0].images;
+  // bannerTitle = movies[0].title;
+  // bannerText = movies[0].description;
