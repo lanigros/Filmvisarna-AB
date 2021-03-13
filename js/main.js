8b3bb545a5cbdc49dbfce89a677d4adff2d67cb8
udden89 <udd.marcus@gmail.com>
2021-03-13 13:16:14 +0100

    Just a test file

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 //Building the body constructor
 $('body').append('<div class="body_container"></div>');
 
+//Building the main content 
+$('.body_container').append('<div class="main_content"></div>');
+$('.main_content').append('<div class="highlighed_movie"></div>');
+$('.highlighed_movie').append('<img src="/img/movieimg/LordOfTheRings-Banner.jpg"></img>');
+$('.highlighed_movie').append('<div class="highlighed_movie_info"></div>');
+$('.highlighed_movie_info').append('<p>Hejdgsgdfhådhdf</p>');
+
 //Building the footer
 $.getScript("/js/footer.js", function () { renderFooter(); })
 
@@ -9,4 +16,3 @@ $.getScript("/js/footer.js", function () { renderFooter(); })
 $('.body_container').append(`<div class="arrowUp" >
     <a href="#"><img onmouseover="this.src='/img/logo/arrowUp40x40-red.png'" onmouseout="this.src='/img/logo/arrowUp40x40.png'" src="/img/logo/arrowUp40x40.png"></a>
       </div>`)
-
