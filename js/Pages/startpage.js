e29037f8b6ef8f1c5b0046aa70d4201f81761ad5
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-14 15:37:22 +0100

    Update startpage.js

--- js/Pages/startpage.js
+++ js/Pages/startpage.js
@@ -1,22 +1,28 @@
 export default class StartPage {
    
+    
+
+    
   render() {
-  
+
+   async function getMovieInfoFromJson() {
+    movieInfo= await $.getJSON('/json/movies.json')
+}
+    getMovieInfoFromJson()
+    
     return /*html*/ `
-      .
+  <div class="banner-Container">
+  <img src="${movieInfo[0].images}">
+    <div class="banner-text">
+      <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
+      <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
+    </div>
+        <button class="order-btn">Se trailer</button>
+    
+</div>
   `
   }
 
 }
 
 
-
-//   <div class="banner-Container">
-//   <img src="${movieInfo[0].images}">
-//     <div class="banner-text">
-//       <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
-//       <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
-//     </div>
-//         <button class="order-btn">Se trailer</button>
-    
-// </div>
