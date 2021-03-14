0f68913c1ad6a59482b6c2353a813aebbfff83f0
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-14 21:59:13 +0100

    Update startpage.js

--- js/Pages/startpage.js
+++ js/Pages/startpage.js
@@ -1,25 +1,36 @@
 export default class StartPage {
    
-    
+  //JSON._load
 
-    
+  async read() {
+    // Read data from a JSON file
+    this.movieInfo = await $.getJSON('/json/movies.json');
+    console.log('Read');
+    console.table(this.movieInfo);
+  }
+
+        
   render() {
 
    async function getMovieInfoFromJson() {
-    movieInfo= await $.getJSON('/json/movies.json')
+    let movieInfo= await $.getJSON('/json/movies.json')
 }
-    getMovieInfoFromJson()
+    // getMovieInfoFromJson()
     
     return /*html*/ `
-  <div class="banner-Container">
-  <img src="${movieInfo[0].images}">
+  
+ <div class="banner-Container">
+    ${this.movieInfo[0].images};
     <div class="banner-text">
-      <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
-      <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
+      <div class="banner-title"><h1>${this.movieInfo[0].title}</h1></div>
+      <div class="banner-desc"><p>${this.movieInfo[0].description}</p></div>
     </div>
         <button class="order-btn">Se trailer</button>
     
-</div>
+</div> 
+
+    
+  
   `
   }
 
