52574dd90fba1889442523480b32652ce5ca58f6
udden89 <udd.marcus@gmail.com>
2021-03-15 13:36:48 +0100

    Start of the the page that contains detailed info about a movie

--- js/Pages/detailedInfoAboutMovies.js
+++ js/Pages/detailedInfoAboutMovies.js
@@ -14,15 +14,23 @@ export default class DetailedInfoAboutMovie {
 
     return /*html*/ `
     
-      <div class="detailedInfoAboutMovie-Container">      
-        <img src="${this.movieInfo[0].images}">
+      <div class="detailedInfoAboutMovie-Container">
+
+        <div class="banner_movie_container">    
+          <img src="${this.movieInfo[0].images}">
+        </div>
+
+        <div class="movie_title"><h1>${this.movieInfo[0].title}</h1></div>
+        <div class="movie_duration_lenght"><h1>${this.movieInfo[0].lenght}</h1></div>
+        <div class="movie_genre"><h1>${this.movieInfo[0].genre}</h1></div>
+
+
           <div class="movie_text">
-            <div class="movie_title"><h1>${this.movieInfo[0].title}</h1></div>
+            
             <div class="movie_desc"><p>${this.movieInfo[0].description}</p></div>
             <button class="order-btn">Se trailer</button>
           </div> 
 
         </div>`
-
   }
 }
