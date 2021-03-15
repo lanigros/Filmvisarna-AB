d638355331da7ac489e98c087eec57e105a123ef
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-15 08:00:14 +0100

    Removed the component frontpage-banner
    due to the lack in size would not work if i would have written it as its own class, i decided to extract the code and directly put it in startpage(default)

--- js/Pages/startpage.js
+++ js/Pages/startpage.js
@@ -1,28 +1,23 @@
 export default class StartPage {
-   
   //JSON._load
 
   async read() {
     // Read data from a JSON file
-    this.movieInfo = await $.getJSON('/json/movies.json');
-    return;
-  }
+    this.movieInfo = await JSON._load('/json/movies.json');
+    return this.movieInfo;
 
+  }
         
   render() {
   
-  //  async function getMovieInfoFromJson() {
-  //   this.movieInfo= await $.getJSON('/json/movies.json')
-//}
-    // getMovieInfoFromJson()
     
     return /*html*/ `
   
  <div class="banner-Container">
-    ${this.movieInfo[0].images};
+   
     <div class="banner-text">
-      <div class="banner-title"><h1>${this.movieInfo[0].title}</h1></div>
-      <div class="banner-desc"><p>${this.movieInfo[0].description}</p></div>
+      <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
+      <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
     </div>
         <button class="order-btn">Se trailer</button>
     
@@ -36,3 +31,7 @@ export default class StartPage {
 }
 
 
+ //  async function getMovieInfoFromJson() {
+  //   this.movieInfo= await $.getJSON('/json/movies.json')
+//}
+    // getMovieInfoFromJson()
