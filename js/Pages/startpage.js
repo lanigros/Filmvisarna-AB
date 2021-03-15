e56ab95c32e78d230b1454bc940044bed3127062
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-15 11:06:33 +0100

    Fixed a few issues with the startpage

--- js/Pages/startpage.js
+++ js/Pages/startpage.js
@@ -2,30 +2,30 @@ export default class StartPage {
   
 
   async read() {
-    let movieInfo = await $.getJSON('/json/movies.json');
-    console.log(movieInfo[0].title);
-
-  }
-        
-  render() {
+    this.movieInfo = await $.getJSON('/json/movies.json');
     
-    this.read();
+  }
+
+  
+  async render() {
+
+    console.log('rendering')
+    if (!this.movieInfo) {
+      await this.read()
+    }
     
     return /*html*/ `
   
  <div class="banner-Container">
-   <img src="${movieInfo[0].images}">
+   <img src="${this.movieInfo[0].images}">
     <div class="banner-text">
-      <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
-      <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
+      <div class="banner-title"><h1>${this.movieInfo[0].title}</h1></div>
+      <div class="banner-desc"><p>${this.movieInfo[0].description}</p></div>
     </div>
         <button class="order-btn">Se trailer</button>
     
-</div> 
-
-    
+</div>`
   
-  `
   }
 
 }
