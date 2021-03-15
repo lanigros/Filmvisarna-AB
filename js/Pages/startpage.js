594b6e406ed2017b16fcf23ce239b699af0a8573
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-15 14:58:20 +0100

    major fixes
    Since we switched structre to grid, alot of the content already pre-written became broken.

--- js/Pages/startpage.js
+++ js/Pages/startpage.js
@@ -17,10 +17,10 @@ export default class StartPage {
     return /*html*/ `
   
  <div class="banner-Container">
-   <img src="${movieInfo[0].images}">
+   <img src="${this.movieInfo[0].images}">
     <div class="banner-text">
-      <div class="banner-title"><h1>${movieInfo[0].title}</h1></div>
-      <div class="banner-desc"><p>${movieInfo[0].description}</p></div>
+      <div class="banner-title"><h1>${this.movieInfo[0].title}</h1></div>
+      <div class="banner-desc"><p>${this.movieInfo[0].description}</p></div>
     </div>
         <button class="order-btn">Se trailer</button>
     
