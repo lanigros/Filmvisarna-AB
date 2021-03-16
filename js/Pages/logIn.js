13f93e7574eeffc62506d2d9498190623d231123
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-16 13:45:39 +0100

    Starting with the loginpage

--- js/Pages/logIn.js
+++ js/Pages/logIn.js
@@ -5,7 +5,17 @@ export default class LogIn{
     return `
     
     <div class="Login-Container">
-    
+    <div class="Title-Container"><h1>login</h1></div>
+      
+      <div class="Input-Container>
+            <form action="/action_page.php">
+              <label for="email">Enter email</label>
+              <input type="Email" id="email" name="fname"><br><br>
+              <label for="pswrd">Enter password</label>
+              <input type="password" id="pswrd" name="lname"><br><br>
+              <input type="submit" value="Submit">
+            </form>
+      </div>
     
     </div>
     
