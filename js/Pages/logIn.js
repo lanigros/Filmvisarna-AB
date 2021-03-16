429cb13bf4a37735b500800afa2397f93eddfc67
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-16 11:42:48 +0100

    Fixed the navbar
    In order for our grid to work i needed to change a few css settings to that the navbar would get inside the grid instead of being "not part of the grid"

--- js/Pages/logIn.js
+++ js/Pages/logIn.js
@@ -1,9 +1,13 @@
 export default class LogIn{
 
   render() {
-    console.log('Helloo 2')
+  
     return `
-    <h1>Hello</h1>
+    
+    <div class="Login-Container">
+    
+    
+    </div>
     
     `
    
