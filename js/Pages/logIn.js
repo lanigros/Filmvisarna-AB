50cf83a90c502fad0daf5bf6e7855883c9d8a5a5
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-16 15:19:28 +0100

    Styling Loginpage

--- js/Pages/logIn.js
+++ js/Pages/logIn.js
@@ -5,18 +5,22 @@ export default class LogIn{
     return `
     
     <div class="Login-Container">
+    <div class="Login-wrapper">
     <div class="Title-Container"><h1>login</h1></div>
       
-      <div class="Input-Container>
-            <form action="/action_page.php">
-              <label for="email">Enter email</label>
-              <input type="Email" id="email" name="fname"><br><br>
-              <label for="pswrd">Enter password</label>
-              <input type="password" id="pswrd" name="lname"><br><br>
+      <div class="Input-Container">
+            <form>
+
+              
+              <input type="Email" placeholder="Email adress" id="email" name="email">
+              <br>
+              
+              <input type="password" placeholder="Password" id="pswrd" name="pswrd">
+              <br>
               <input type="submit" value="Submit">
             </form>
-      </div>
-    
+        </div>
+     </div>
     </div>
     
     `
