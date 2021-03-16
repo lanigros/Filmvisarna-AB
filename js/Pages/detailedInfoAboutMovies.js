0d0ef1d1e5b36e6e61d44e0c1af51da94e66ab27
udden89 <udd.marcus@gmail.com>
2021-03-16 08:49:38 +0100

    Got Yoube trailer working

--- js/Pages/detailedInfoAboutMovies.js
+++ js/Pages/detailedInfoAboutMovies.js
@@ -23,14 +23,19 @@ export default class DetailedInfoAboutMovie {
         <div class="movie_title"><h1>${this.movieInfo[0].title}</h1></div>
         <div class="movie_duration_lenght"><h1>${this.movieInfo[0].lenght}</h1></div>
         <div class="movie_genre"><h1>${this.movieInfo[0].genre}</h1></div>
+       
+        <div class="movie_trailer_container">
+          <!-- Copy & Pasted from YouTube -->
+          <iframe width="560" height="349" src="${this.movieInfo[0].youtubeTrailers}" frameborder="0" allowfullscreen></iframe>
+        </div>
 
-
-          <div class="movie_text">
-            
-            <div class="movie_desc"><p>${this.movieInfo[0].description}</p></div>
-            <button class="order-btn">Se trailer</button>
-          </div> 
-
-        </div>`
+        <div class="movie_desc_container"><p>${this.movieInfo[0].description}</p>
+        </div>
+        
+        <button class="order-btn">Se trailer</button>
+        
+        
+        
+        `
   }
 }
