09cf6a1c6f99c56fd7713007c845f2bfeac20ca9
udden89 <udd.marcus@gmail.com>
2021-03-18 20:17:50 +0100

    Finally got the calander syncing in real life!

--- js/components/elementsInStartPage/calendar.js
+++ js/components/elementsInStartPage/calendar.js
@@ -1,4 +1,7 @@
-import { isWeekend, getDaysInMonth, getCurrentMonthInNumber, getCurrentMonthInString, getCurrentYear } from "./date-helper.js";
+import { isWeekend, getDaysInMonth, getCurrentMonthInNumber, getCurrentMonthInString, getCurrentYear, getThePickedMonth, calcStartDayOfSpecificMonth } from "./date-helper.js";
+
+var pickedMonthInCalandar = getCurrentMonthInNumber();
+console.log("start:", pickedMonthInCalandar)
 
 export default class Calendar {
 
@@ -20,8 +23,6 @@ export default class Calendar {
     this.buildCalendar();
   }
 
-
-
   buildCalendar() {
 
     $("main").append(`
@@ -33,7 +34,7 @@ export default class Calendar {
 
     $('.calendar').append(`<div class="selectedMonth_container"></div>`);
     $('.selectedMonth_container').append(`<button class="btn_previousMonth"> < show previous month </button>`);
-    $('.selectedMonth_container').append(`<div class="selectedMonth" value="${getCurrentMonthInNumber() + 1}"> ${getCurrentMonthInString()} </div>`);
+    $('.selectedMonth_container').append(`<div class="selectedMonth" value="${getCurrentMonthInNumber() + 1}"> ${getThePickedMonth(pickedMonthInCalandar)} </div>`);
     $('.selectedMonth_container').append(`<button class="btn_nextMonth"> show next month> </button>`);
 
     $('.calendar').append(`<div class="nameOfDay"> Mon </div>`);
@@ -44,13 +45,13 @@ export default class Calendar {
     $('.calendar').append(`<div class="nameOfDay"> Sat </div>`);
     $('.calendar').append(`<div class="nameOfDay"> Sun </div>`);
 
-    this.renderDatesInCalendar(getDaysInMonth(getCurrentYear(), getCurrentMonthInNumber() + 1), getCurrentMonthInNumber());
+    this.renderDatesInCalendar(pickedMonthInCalandar);
   }
 
   eventHandeler() {
     $('main').on("click", ".btn-show-calender", (event) => this.renderSaloonInfo(event));
-    $('main').on("click", ".btn_previousMonth", (event) => this.pressBackPreviousMonth(event));
-    $('main').on("click", ".btn_nextMonth", (event) => this.renderSaloonInfo(event));
+    $('main').on("click", ".btn_previousMonth", (event) => this.pressingBtnPreviousMonth(event));
+    $('main').on("click", ".btn_nextMonth", (event) => this.pressingBtnNextMonth(event));
   }
 
   renderSaloonInfo(event) {
@@ -69,44 +70,73 @@ export default class Calendar {
 
   }
 
-  pressBackPreviousMonth() {
+  pressingBtnPreviousMonth() {
 
-    let currentMonth = $('.selectedMonth').val("value");
-    console.log(currentMonth);
+    if (pickedMonthInCalandar > 1 && pickedMonthInCalandar <= 13) {
 
+      $('.day').replaceWith("");
+      $('.notAvailable').replaceWith("");
 
-    this.renderDatesInCalendar();
+      pickedMonthInCalandar--;
+      calcStartDayOfSpecificMonth(pickedMonthInCalandar);
+      $('.selectedMonth').html(`<div class="selectedMonth" value="${getCurrentMonthInNumber() + 1}"> ${getThePickedMonth(pickedMonthInCalandar)} </div>`);
 
+      this.renderDatesInCalendar(pickedMonthInCalandar);
+
+    }
 
   }
 
-  renderDatesInCalendar(howManyDaysInMonth, selectedMonth) {
+  pressingBtnNextMonth() {
+
+    if (pickedMonthInCalandar >= 1 && pickedMonthInCalandar < 12) {
+
+      $('.day').replaceWith("");
+      $('.notAvailable').replaceWith("");
+
+      pickedMonthInCalandar++;
+      $('.selectedMonth').html(`<div class="selectedMonth" value="${getCurrentMonthInNumber() + 1}"> ${getThePickedMonth(pickedMonthInCalandar)} </div>`);
+
+      this.renderDatesInCalendar(pickedMonthInCalandar);
+
+    }
+  }
+
+  //This function calculates how many days it should render per month. It also calculates what weekday the month starts with. 
+  renderDatesInCalendar(selectedMonth) {
     console.log("Fired from renderDatesInCalendar");
-    console.log(howManyDaysInMonth);
+    console.log("SelectedMonth = ", selectedMonth)
+
+    let howManyDaysToLoop = getDaysInMonth(selectedMonth);
+    console.log("howManyDaysInMonth:", howManyDaysToLoop)
+
+    let howManyDaysToSkip = calcStartDayOfSpecificMonth(selectedMonth);
+    console.log("howManyDaysToSkip: ", howManyDaysToSkip)
 
     let dates = [];
-    console.log("fhda", dates)
+    console.log("Array", dates)
 
     let day = 0;
 
+    for (let i = howManyDaysToSkip; i > 0; i--) {
+      $('.calendar').append(`<div class="notAvailable"> - </div>`);
+    }
+
     //Adding all 31 days in month into "calendar_Container" with own divs.
-    for (let i = 1; i <= howManyDaysInMonth; i++) {
+    for (let i = 1; i <= 31; i++) {
 
       day++;
 
-      /* if (i < 4) {
-        $('.calendar').append(`<div class="notAvailable">  </div>`);
-        day--;
-        continue;
-      } */
-
       const weekend = isWeekend(day);
 
-      let datum = new Date(getCurrentYear(), selectedMonth, day + 1).toISOString().split('T')[0];
+      //Converts the date to format "2021-04-01" wich is needed for JSON
+      let datum = new Date(getCurrentYear(), selectedMonth - 1, day + 1).toISOString().split('T')[0];
 
       dates.push(datum);
 
       $('.calendar').append(`<div class="day ${weekend ? "weekend" : ""}"> <button value="${datum}" class="btn-show-calender">${day}</button> </div>`);
+
+
     }
   }
 
