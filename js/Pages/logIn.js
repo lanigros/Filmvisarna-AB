e424e501edf6cb62a4c3582cdea0077ae5589029
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-18 13:05:14 +0100

    Finished with the styling
    I've decided to have the log in and register a new account on the same page.
    Done with the styling, now i have to create the json file that stores the information from register

--- js/Pages/logIn.js
+++ js/Pages/logIn.js
@@ -5,21 +5,44 @@ export default class LogIn{
     return `
     
     <div class="Login-Container">
-    <div class="Login-wrapper">
-    <div class="Title-Container"><h1>login</h1></div>
+      <div class="Login-wrapper">
+        <div class="Title-Container"><h1>login</h1></div>
       
-      <div class="Input-Container">
+        <div class="Input-Container">
             <form>
-              <input type="Email" placeholder="Email adress" id="email" name="email" required>
+              <input type="Email" placeholder="Email adress" id="log-email" name="email" required>
               <br>
-              <input type="password" placeholder="Password" id="pswrd" name="pswrd" required>
+              <input type="password" placeholder="Lösenord" id="log-pswrd" name="pswrd" required>
               <br>
               <input type="submit" value="Logga in" id="sub-btn">
             </form>
         </div>
      </div>
-    </div>
     
+
+      <div class="box-divider"></div>
+
+  
+      <div class="Register-wrapper">
+      <div class="Title-Container"><h1>Nytt konto</h1></div>
+      
+      <div class="Input-Container">
+                <form>
+                  <input type="Email" placeholder="Email adress" id="crt-email" name="email" required>
+                  <br>
+                  <input type="password" placeholder="Lösenord (8-16 tecken)" id="crt-pswrd" name="pswrd" required>
+                  <br>
+                  <input type="Text" placeholder="Förnamn" id="fname" name="fname" required>
+                  <br>
+                  <input type="Text" placeholder="Efternamn" id="lname" name="lname" required>
+                  <br>
+                  <input type="submit" value="Skapa" id="crt-btn">
+                </form>
+              </div>
+          </div>
+        </div>
+      </div>
+   </div> 
     `
    
   }
