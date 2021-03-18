f30c99b62292533aed9f1ace08ee0c549b0babbb
udden89 <udd.marcus@gmail.com>
2021-03-18 16:16:38 +0100

    Revert "Merge branch 'main' into Calender_testing_functions"
    This reverts commit 1c9fe864e3db2ebfb0d8e8d8f35dc397f5c07659, reversing
    changes made to 597e520894c8df8e35b4be20844280c2b21760d2.

--- js/Pages/logIn.js
+++ js/Pages/logIn.js
@@ -5,19 +5,8 @@ export default class LogIn{
     return `
     
     <div class="Login-Container">
-    <div class="Login-wrapper">
-    <div class="Title-Container"><h1>login</h1></div>
-      
-      <div class="Input-Container">
-            <form>
-              <input type="Email" placeholder="Email adress" id="email" name="email" required>
-              <br>
-              <input type="password" placeholder="Password" id="pswrd" name="pswrd" required>
-              <br>
-              <input type="submit" value="Logga in" id="sub-btn">
-            </form>
-        </div>
-     </div>
+    
+    
     </div>
     
     `
