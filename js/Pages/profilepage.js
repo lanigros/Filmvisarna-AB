2fbd67c1d361d1abc63a7d708c6db746e0f9123c
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-03-23 18:02:09 +0100

    Working on a forloop
    Trying to make the loop render out the information on  the page

--- js/Pages/profilepage.js
+++ js/Pages/profilepage.js
@@ -13,34 +13,47 @@ export default class ProfilePage{
   async render() {
 
     
-    console.log();
+    this.ticketLooper();
 
     if (!this.currentUser) {
       await this.read();
     }
     
-    
-  
     return /*html*/ `
     
     <div class="profile-page-container">
-        <div class="profile-wrapper">
-        <h1 class="profile-title">profil</h1>
-        <div class="profile-text-container">
-        <p>Epost-adress: ${window.activeUser.Email} </p>
-        <p>Namn: ${window.activeUser.Name}</p>
-        <p>Efternamn: ${window.activeUser.Lastname}</p>
-        </div>
-        </div>
-        <div class="profile-divider"></div>
-        <div class="bookings-wrapper">
-        <h1 class="bookings-title">bokningar</h1>
-        <div class="bookings-text-container">
-        <p class="bookedTicketInfo"></p>
-        <p>(Avboka knapp)</p>
-        </div>
-        </div>
+          <div class="profile-wrapper">
+          <h1 class="profile-title">profil</h1>
+          <div class="profile-text-container">
+          <p>Epost-adress: ${window.activeUser.Email} </p>
+          <p>Namn: ${window.activeUser.Name}</p>
+          <p>Efternamn: ${window.activeUser.Lastname}</p>
+          </div>
+          </div>
+          <div class="profile-divider"></div>
+          <div class="bookings-wrapper">
+            <h1 class="bookings-title">bokningar</h1>
+            <div class="bookings-text-container"></div>
+              
+              <p>(Avboka knapp)</p>
+          </div>
+          </div>
     </div>
     `
+  }
+
+  ticketLooper() {
+    let user = window.activeUser;
+    
+    for (let i = 0; i < user.bookedShows.length; i++){
+      $('.bookings-text-container').append( /*html*/ `
+      <div class=tickets></div>`)
+      bookedShows.forEach(ticketInfo => {
+        $('.tickets').append(/*html*/ `
+        <p>${ticketInfo}</p>
+        `)
+       });
+      return;
+    }
   }
 }
